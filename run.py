6c49e5e9305e81d3505c2e751cbb269ae70feb9c
Takumi <taku@tak3mac.local>
2023-11-07 18:58:08 +0900

    [init] init code

---
+++ run.py
@@ -0,0 +1,61 @@
+import argparse
+
+import base64
+import requests
+
+from settings.setting import API_KEY
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-i', '--image', type=str,)
+    parser.add_argument('-p', '--prompt', type=str)
+    return parser.parse_args()
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+    
+
+def main(args: argparse.Namespace):
+    # Getting the base64 string
+    base64_image = encode_image(args.image)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {API_KEY}"
+    }
+
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+          {
+            "role": "user",
+            "content": [
+              {
+                "type": "text",
+                "text": args.prompt,
+              },
+              {
+                "type": "image_url",
+                "image_url": {
+                  "url": f"data:image/jpeg;base64,{base64_image}"
+                }
+              }
+            ]
+          }
+        ],
+        "max_tokens": 300
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    # print(response.json())
+    print("*"*50)
+    print(response.json()['choices'][0]['message']['content'])
+    print("*"*50)
+
+if __name__ == "__main__":
+    main(parse_args())
